1f5bf8c44d7f3a778fdb6886900ff81fef39b296
Simon Onyszczak Hansen <simo401t@zbc.dk>
2025-03-12 15:42:29 +0100

    made the search function, but it does not work properly(WIP)

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,18 +1,70 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { TmdbService } from '../../services/tmdb.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+interface SearchResult {
+  id: number;
+  media_type: 'movie' | 'tv' | 'person';
+  title?: string;
+  name?: string;
+  poster_path?: string;
+  profile_path?: string;
+}
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
   searchQuery: string = '';
+  searchResults: any[] = [];
+  showResults: boolean = false;
   isMobileMenuOpen: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private tmdbService: TmdbService) { }
+
+  onSearchInput() {
+    if (this.searchQuery.length > 2) {
+      this.tmdbService.searchMulti(this.searchQuery).subscribe({
+        next: (response: any) => {
+          this.searchResults = response.results
+            .filter((item: SearchResult) => ['movie', 'tv', 'person'].includes(item.media_type))
+            .slice(0, 5);
+          this.showResults = true;
+        },
+        error: (err) => console.error('Search error:', err)
+      });
+    } else {
+      this.searchResults = [];
+      this.showResults = false;
+    }
+  }
+
+  navigateToResult(result: SearchResult) {
+    this.showResults = false;
+    this.searchQuery = '';
+
+    switch(result.media_type) {
+      case 'movie':
+        this.router.navigate(['/movie', result.id, result.title]);
+        break;
+      case 'tv':
+        this.router.navigate(['/tv', result.id, result.name]);
+        break;
+      case 'person':
+        this.router.navigate(['/actor', result.id, result.name]);
+        break;
+    }
+  }
+
+  closeSearchResults() {
+    this.showResults = false;
+  }
 
   goToHome() {
     this.router.navigate(['/home'])
@@ -44,6 +96,6 @@ export class NavbarComponent {
   }
 
   onSearch() {
-    
+
   }
 }
