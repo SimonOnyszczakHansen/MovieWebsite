87da3eaf66dd4c1f7deac9aaf503214c84858ba2
Simon Onyszczak Hansen <simo401t@zbc.dk>
2025-03-13 15:44:44 +0100

    person details(WIP)

--- src/app/person-details/person-details.component.ts
+++ src/app/person-details/person-details.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { TmdbService } from '../../services/tmdb.service';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-person-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './person-details.component.html',
   styleUrl: './person-details.component.css'
 })
-export class PersonDetailsComponent {
+export class PersonDetailsComponent implements OnInit {
+  personId!: number;
+  personInfo: any[] = [];
+  private routeSub!: Subscription
 
+  constructor(private tmdbService: TmdbService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.routeSub = this.route.params.subscribe(params => {
+      this.personId = +params['id']
+    })
+    this.getPersonDetails()
+  }
+
+  ngOnDestroy() {
+    this.routeSub
+  }
+
+  getPersonDetails() {
+    this.tmdbService.getPersonDetails(this.personId).subscribe(data => {
+      this.personInfo = data;
+    })
+  }
 }
