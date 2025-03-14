e4098a80f92b7f5d5bf2add0e9e8bd858b492653
Simon Onyszczak Hansen <simo401t@zbc.dk>
2025-03-14 09:28:30 +0100

    smol adjustments to buttons and stuff

--- src/app/person-details/person-details.component.ts
+++ src/app/person-details/person-details.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { TmdbService } from '../../services/tmdb.service';
 import { Subscription } from 'rxjs';
 import { CommonModule } from '@angular/common';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-person-details',
@@ -14,9 +15,10 @@ import { CommonModule } from '@angular/common';
 export class PersonDetailsComponent implements OnInit {
   personId!: number;
   person: any;
+  combinedCredits: any[] = [];
   private routeSub!: Subscription
 
-  constructor(private tmdbService: TmdbService, private route: ActivatedRoute) { }
+  constructor(private tmdbService: TmdbService, private route: ActivatedRoute, private location: Location) { }
 
   ngOnInit(): void {
     this.routeSub = this.route.params.subscribe(params => {
@@ -33,5 +35,15 @@ export class PersonDetailsComponent implements OnInit {
     this.tmdbService.getPersonDetails(this.personId).subscribe(data => {
       this.person = data;
     })
+
+    this.tmdbService.getPersonCombinedCredits(this.personId).subscribe(credits => {
+      this.combinedCredits = credits.cast.sort((a: any, b: any) => 
+        new Date(b.release_date).getTime() - new Date(a.release_date).getTime()
+      )
+    })
+  }
+
+  goBack() {
+    this.location.back()
   }
 }
